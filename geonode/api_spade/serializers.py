a4000d6a62d9ac51d4752a8cdc0d5b472ff30e93
Pierre HARDY <pierre.hardy@egis.fr>
2022-08-25 08:43:57 +0000

    fix(api): modification of serialiser to remove ADB categories

--- geonode/api_spade/serializers.py
+++ geonode/api_spade/serializers.py
@@ -28,7 +28,6 @@ class ResourceSerializer(serializers.ModelSerializer):
         fields = ['pk']
 
 class ResourceDetailSerializer(serializers.ModelSerializer):
-    adb_themes = ADBThemeSerializer(many=True)
     class Meta:
         model = ResourceBase
-        fields = ['title', 'thumbnail_url' , 'detail_url', 'alternate', 'date', 'date_type', 'raw_data_quality_statement', 'raw_supplemental_information', 'raw_abstract', 'alternate', 'adb_geospatdivision', 'adb_themes', 'bbox_polygon','ll_bbox_polygon']
+        fields = ['title', 'thumbnail_url' , 'detail_url', 'alternate', 'date', 'date_type', 'raw_data_quality_statement', 'raw_supplemental_information', 'raw_abstract', 'alternate', 'adb_geospatdivision', 'regions', 'categories', 'bbox_polygon','ll_bbox_polygon']
